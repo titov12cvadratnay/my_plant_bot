85e40b036f496356129161559c37c730b9479113
titov12cvadratnay <titovmatvej16@gmail.com>
2025-05-21 08:52:34 +0300

    Update plant_bot.py

--- plant_bot.py
+++ plant_bot.py
@@ -23,17 +23,6 @@ cursor.execute('''
     )
 ''')
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 const express = require('express');
 const app = express();
